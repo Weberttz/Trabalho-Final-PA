6b389df3711979899816a62202f1d8b162cce5ff
jsfael <faeulimao@gmail.com>
2025-11-20 16:03:55 -0300

    feat: Refatoração e testes com função trocarVez

--- include/jogo.h
+++ include/jogo.h
@@ -2,38 +2,10 @@
 #define JOGO_H_
 
 #include "tabuleiro.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-void novojogo(){
-    montarTabuleiros();
-    imprimirTabuleiro(); 
-}
+void novojogo();
+int trocarVez(int vezAtual);
+void clear();
+void instrucoes();
 
-void clear() {
-    #ifdef _WIN32
-    system("cls");
-    #else 
-    system("clear");
-    #endif
-}
-
-void instrucoes(){
-    FILE *arquivo; 
-    int resposta = 1;
-    inicio_instrucoes:
-    arquivo = fopen("../data/instrucoes.txt", "r");
-    char linha[100];
-    while (fgets(linha, sizeof(linha), arquivo))
-    {    
-    printf("%s", linha);
-    }
-    
-    while(resposta != 0){
-        printf("\nDigite 0 para voltar.\n");
-        scanf("%d", &resposta);
-        clear();
-        goto inicio_instrucoes;
-    }
-}
 #endif
